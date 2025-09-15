ecccc06333c9004daae073768cf3800024910369
Assil22112001 <assilfous5@gmail.com>
2025-09-15 05:35:29 +0300

    naitve splash done

---
+++ lib/bookia.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:bookia_app/feature/welcome/presentation/ui/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+class Bookia extends StatelessWidget {
+  const Bookia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: WelcomeScreen(),
+    );
+  }
+}
